d983ae04a62ae52706119859a6fa8d64a25cc6fb
brahem-ayad <br4hemayad@gmail.com>
2025-11-10 10:07:21 +0300

    Calculate Signed distance between a moving point and a line section and Implement a continous Hit Detection

--- tests/signed_distance/signed_distances.cpp
+++ tests/signed_distance/signed_distances.cpp
@@ -1,7 +1,9 @@
 #include "raylib.h"
 #include "raymath.h"
+#include <algorithm>
 
 // From the Video : https://youtu.be/KHuI9bXZS74?si=2h0ZbzMvoj_7QBtT
+// And this Video : https://youtu.be/LyQWZRfWotQ?si=DsBpO_zhN__4HOR-
 
 class Wall{
   public:
@@ -23,23 +25,62 @@ class Wall{
 class Point{
   public:
     Vector2 Position;
+    Vector2 Previous_Position;
 
     Point(Vector2 Position) {
       this->Position = Position;
+      this->Previous_Position = Position;
     }
 
     void Draw() {
       DrawCircleV(Position, 5, RED);
+      DrawCircleV(Previous_Position, 5, {100, 0, 0, 255});
     }
 
-    float Distance_to_Line(Vector2 Line_Start, Vector2 Line_Normal){
+    float Distance_to_Line(Vector2 pos, Vector2 Line_Start, Vector2 Line_End, Vector2 Line_Normal){
 
-      Vector2 Line_Start_to_Point = {Position.x - Line_Start.x, Position.y - Line_Start.y};
+      Vector2 Line_Start_to_Point = {pos.x - Line_Start.x, pos.y - Line_Start.y};
 
-      float distance = Vector2DotProduct(Line_Start_to_Point, Line_Normal);
+      Vector2 Line_Vector = {Line_End.x - Line_Start.x, Line_End.y - Line_Start.y};
 
-      return distance;
+      float t = std::clamp(Vector2DotProduct(Line_Start_to_Point, Line_Vector)/Vector2DotProduct(Line_Vector, Line_Vector), 0.0f, 1.0f);
+
+      Vector2 Closest = Vector2Add(Line_Start, Vector2Scale(Line_Vector, t));
+
+      DrawLineV(Position, Closest, YELLOW);
+
+      float distance = Vector2Distance(pos, Closest);
+
+      float crossVal = (Line_Vector.x * Line_Start_to_Point.y - Line_Vector.y * Line_Start_to_Point.x);  // 2D cross product (scalar)
+      float sign = (crossVal < 0.0) ? -1.0 : 1.0;
+
+      return sign * distance;
     }
+
+    void HitDetection(Vector2 Line_Start, Vector2 Line_End, Vector2 Line_Normal) {
+
+      Vector2 Line_Vector = {Line_End.x - Line_Start.x, Line_End.y - Line_Start.y};
+
+      float d0 = Distance_to_Line(Previous_Position, Line_Start, Line_End, Line_Normal);
+      float d1 = Distance_to_Line(Position, Line_Start, Line_End, Line_Normal);
+
+      float t = d0 / (d0 - d1);
+      Vector2 motion = {Position.x - Previous_Position.x, Position.y - Previous_Position.y};
+      Vector2 hitPos = Vector2Add(Previous_Position, Vector2Scale(motion, t) );
+
+      bool hit;
+
+      float segParam = Vector2DotProduct({hitPos.x - Line_Start.x, hitPos.y - Line_Start.y}, Line_Vector) / Vector2DotProduct(Line_Vector, Line_Vector);
+      if (segParam >= 0.0 && segParam <= 1.0 && d0*d1 < 0 or d1 == 0) {
+          hit = true;   // within endpoints
+          DrawText("HIT Detected", 10, 70, 25, RED);
+      }
+      else {
+          hit = false;  // missed — passed above/below
+      }
+
+    }
+
 };
 
 int main(){
@@ -53,12 +94,17 @@ int main(){
     BeginDrawing();
     ClearBackground(BLACK);
 
-    point.Position = GetMousePosition();
+    if (IsKeyPressed(KEY_SPACE)){
+      point.Previous_Position = point.Position;
+      point.Position = Vector2Add(point.Position, {60, 0});
+    }
 
     wall.Draw();
     point.Draw();
 
-    DrawText(TextFormat("Distance = %f", point.Distance_to_Line(wall.Start, wall.Normal)), 10, 10, 25, BLUE);
+    DrawText(TextFormat("Distance = %f", point.Distance_to_Line(point.Position, wall.Start, wall.End, wall.Normal)), 10, 10, 25, BLUE);
+
+    point.HitDetection(wall.Start, wall.End, wall.Normal);
 
     EndDrawing();
   }
